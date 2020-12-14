6e705e167372cf79e1119180364b2784cb8f1253
Christian Nagel <christian.nagel@tobit.com>
2020-12-14 13:47:27 +0100

    :construction: Improve request helper user agent

--- ChaynsHelper/RequestHelper/RequestHelperExtension.cs
+++ ChaynsHelper/RequestHelper/RequestHelperExtension.cs
@@ -1,5 +1,9 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Reflection;
+using System.Text;
+using Chayns.Auth.Shared.Extensions;
+using Chayns.Auth.Shared.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ChaynsHelper.RequestHelper
@@ -8,14 +12,41 @@ namespace ChaynsHelper.RequestHelper
     {
         public static void AddRequestHelper(this IServiceCollection services)
         {
-            services.AddHttpClient<IHttpClientFactory>("RequestHelper", client =>
+            services.AddHttpClient<RequestHelper>("RequestHelper", client =>
                 client.AddRequestHelperDefaults());
             services.AddSingleton<IRequestHelper, RequestHelper>();
         }
 
         public static void AddRequestHelperDefaults(this HttpClient client)
         {
-            client.DefaultRequestHeaders.Add("User-Agent", Assembly.GetEntryAssembly()?.GetName().Name);
+            client.DefaultRequestHeaders.Add("User-Agent", GetDefaultUserAgent());
+        }
+
+        /// <summary>
+        /// Create a default user-agent based on current assembly and version
+        /// </summary>
+        /// <returns>String in format "ApplicationName/1.0 (Microsoft Windows 10; SERVER-01; .NET Core 3.1)"</returns>
+        private static string GetDefaultUserAgent()
+        {
+            var sb = new StringBuilder();
+
+            var projectName = Assembly.GetEntryAssembly()?.GetName().Name;
+
+            if (projectName != null)
+            {
+                sb.Append(projectName);
+                sb.Append($"/{Assembly.GetEntryAssembly().GetVersion()} ");
+            }
+
+            sb.Append(Assembly.GetExecutingAssembly().GetName().Name + "/");
+            sb.Append(Assembly.GetExecutingAssembly().GetVersion());
+
+            sb.Append(" (");
+            sb.Append(EnvironmentHelper.GetPrettyOsName());
+            sb.Append("; " + Environment.MachineName);
+            sb.Append("; " + EnvironmentHelper.GetPrettyFrameworkName());
+            sb.Append(")");
+            return sb.ToString();
         }
     }
 }
